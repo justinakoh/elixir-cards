f022a844c44613db5a3c20481952b4d3b995132e
justinakoh <justina.y.koh@gmail.com>
2020-11-08 15:21:43 +1300

    Update code to complete exlixir tutorial S3 p29
    This change adds in more tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,6 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffing a deck randomises it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck) #Can also use assert == instead of refute
+  end
 
 end
