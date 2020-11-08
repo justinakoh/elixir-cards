cf0c539084ba3adc8fd07c666555e4807c5e209d
justinakoh <justina.y.koh@gmail.com>
2020-11-08 15:09:18 +1300

    Update code to complete exlixir tutorial S3 p26
    This change adds in some tests for the project

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,8 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "the truth" do
+    assert  1 + 1 == 2
   end
+
 end
